88e3801208f4e3a78b58ba1e631c0d33a9888c01
VictorAcarvalho <carvalho.victorfa@gmail.com>
2021-01-08 15:49:53 -0300

    Corrigindo a criptografia

--- src/app/models/userModel.js
+++ src/app/models/userModel.js
@@ -1,58 +1,30 @@
 const mongoose = require('../../config/db');
 const bcrypt = require('bcryptjs');
-const { Schema } = require('../../config/db');
-const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true,
-        select:false
 
-    },
-    cnpj:{
-        type:Number,
-        required:true
-    },
-    adress:{
-        number:{
-            type:Number,
-            required:true
-        },
-        street:{
-            type:String,
-            required:true
-        },
-        neighborhood:{
-            type:String,
-            required:true
-        },
-        city:{
-          type:String,
-          required:true
-        },
-        state:{
-            type:String,
-            required:true
-        }
-    },
-    cards:[{
-      type:mongoose.Schema.Types.ObjectId,
-      ref:'Cards'
-    }]
+const userSchema = mongoose.Schema(
+    {
+   name:{
+       type:String,
+       required: true
+   },
+   email:{
+    type:String,
+    required: true
 
-    },{timestamps:true})
+   },
+   password:{
+    type:String,
+    required: true,
+    select:false
+   },
+
+},{timestamps:true});
+
 userSchema.pre('save',async function(next){
-  const hash =bcrypt.hash(this.password,10);
-  this.password= hash;
-  next()
+    const hash = await bcrypt.hash(this.password,10);
+    this.password = hash;
+    next();
 });
 
 const user = mongoose.model('User',userSchema);
-module.exports = user;
+module.exports= user
