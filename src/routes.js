0a9a4d8f7cd3ee0f0a28cb2fa328966059499dad
VictorAcarvalho <carvalho.victorfa@gmail.com>
2021-01-15 15:05:26 -0300

    implementa endpoints do cartao digital

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,50 @@
 const userControllers = require('./app/controllers/userControllers');
 const cardControllers = require('./app/controllers/cardControllers');
-const operationControllers = require('./app/controllers/operationController')
+const operationControllers = require('./app/controllers/operationController');
+const digitalCardControllers = require('./app/controllers/digitalControllers');
 const JWT = require('./app/middleware/auth');
 const routes = require('express').Router();
-    //Funcionalidades Usuário
+    //Endpoints Usuário
+
     //cadastra usuário
     routes.post('/user',userControllers.store);
+
     //autentifica o login do usuário
     routes.post('/login',userControllers.auth);
     //json web token
     routes.use(JWT);
 
-    //Funcionalidades do cartão
+
+                               //Endpoints do cartão físico
 
     //cadastrar cartão
     routes.post('/card',cardControllers.storeCard);
+
     //Lista os cartões
     routes.get('/card',cardControllers.list);
+
     //Atualiza o  cartão
     routes.put('/card/:id',cardControllers.update);
+
     //Soft delete
     routes.put('/cardDelete/:id',cardControllers.softDelete);
 
-    //Funcionalidades das transações
+                               //Endpoints catão digital
+
+    //cadastrar catão digital
+    routes.post('/digitalcard',digitalCardControllers.store);
+
+    //listar todos os cartões digitais
+    routes.get('/digitalcard',digitalCardControllers.list);
+
+    //Atualizar um cartão digital
+    routes.put('/digitalcard',digitalCardControllers.update);
+
+    //Soft delete
+    routes.put('/digitalcard',digitalCardControllers.softDelete);
+
+
+                                //Endpoints das transações
 
     //Cria uma nova operação passando o id do cartão como parametro
     routes.post('/operations',operationControllers.store);
